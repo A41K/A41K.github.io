46e05345ac02fb367af032048148469e54b24086
A41K <arokandris@gmail.com>
2025-02-23 21:11:45 +0100

    v2.0 Added Music Player

---
+++ song.js
@@ -0,0 +1,92 @@
+const albums = [
+    {
+        title: "Blonde",
+        cover: "Song/covers/Blonde.png",
+        tracks: [                    
+            "Song/Blonde/Nikes.mp3",
+            "Song/Blonde/Ivy.mp3",
+            "Song/Blonde/Pink+White.mp3",
+            "Song/Blonde/Be Yourself.mp3",
+            "Song/Blonde/Solo.mp3",
+            "Song/Blonde/Skyline To.mp3",
+            "Song/Blonde/Self Control.mp3",
+            "Song/Blonde/Good Guy.mp3",
+            "Song/Blonde/Nights.mp3",
+            "Song/Blonde/Solo (Reprise).mp3",
+            "Song/Blonde/Pretty Sweet.mp3",
+            "Song/Blonde/Facebook Story.mp3",
+            "Song/Blonde/Close to You.mp3",
+            "Song/Blonde/White Ferrari.mp3",
+            "Song/Blonde/Seigfried.mp3",
+            "Song/Blonde/Godspeed.mp3",
+            "Song/Blonde/Futura Free.mp3"]
+    },
+    {
+        
+        title: "DAMN.",
+        cover: "Song/covers/DAMN..png",
+        tracks: [
+            "Song/DAMN/BLOOD.mp3",
+            "Song/DAMN/DNA.mp3",
+            "Song/DAMN/YAH.mp3",
+            "Song/DAMN/ELEMENT.mp3",
+            "Song/DAMN/FEEL.mp3",
+            "Song/DAMN/LOYALTY.mp3",
+            "Song/DAMN/PRIDE.mp3",
+            "Song/DAMN/HUMBLE.mp3",
+            "Song/DAMN/LUST.mp3",
+            "Song/DAMN/LOVE.mp3",
+            "Song/DAMN/XXX.mp3",
+            "Song/DAMN/FEAR.mp3",
+            "Song/DAMN/GOD.mp3",
+            "Song/DAMN/DUCKWORTH.mp3"
+        ]
+    },
+    {
+        title: "Channel Orange",
+        cover: "Song/covers/channelORANGE.png",
+        tracks: [
+            "Song/ChannelOrange/Start.mp3",
+            "Song/ChannelOrange/Thinkin Bout You.mp3",
+            "Song/ChannelOrange/Fertilizer.mp3",
+            "Song/ChannelOrange/Sierra Leone.mp3",
+            "Song/ChannelOrange/Sweet Life.mp3",
+            "Song/ChannelOrange/Not Just Money.mp3",
+            "Song/ChannelOrange/Super Rich Kids.mp3",
+            "Song/ChannelOrange/Pilot Jones.mp3",
+            "Song/ChannelOrange/Crack Rock.mp3",
+            "Song/ChannelOrange/Pyramids.mp3",
+            "Song/ChannelOrange/Lost.mp3",
+            "Song/ChannelOrange/White.mp3",
+            "Song/ChannelOrange/Monks.mp3",
+            "Song/ChannelOrange/Bad Religion.mp3",
+            "Song/ChannelOrange/Pink Matter.mp3",
+            "Song/ChannelOrange/Forrest Gump.mp3",
+            "Song/ChannelOrange/End.mp3"
+        ]
+    },
+    {
+        title: "Mr. Morale",
+        cover: "Song/covers/MrMorale.png",
+        tracks: [
+            "Song/MrMorale/United In Grief.mp3",
+            "Song/MrMorale/N95.mp3",
+            "Song/MrMorale/Worldwide Steppers.mp3",
+            "Song/MrMorale/Die Hard.mp3",
+            "Song/MrMorale/Father Time.mp3",
+            "Song/MrMorale/Rich.mp3",
+            "Song/MrMorale/Rich Spirit.mp3",
+            "Song/MrMorale/We Cry Together.mp3",
+            "Song/MrMorale/Purple Hearts.mp3",
+            "Song/MrMorale/Count Me Out.mp3",
+            "Song/MrMorale/Crown.mp3",
+            "Song/MrMorale/Silent Hill.mp3",
+            "Song/MrMorale/Savior - Interlude.mp3",
+            "Song/MrMorale/Savior.mp3",
+            "Song/MrMorale/Auntie Diaries.mp3",
+            "Song/MrMorale/Mr. Morale.mp3",
+            "Song/MrMorale/Mother I Sober.mp3",
+            "Song/MrMorale/Mirror.mp3"
+        ]
+    }
+];
